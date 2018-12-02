d54698d7372828f335f47633dcff8a25140df6ee
jakubiakm <maciej190509@gmail.com>
2018-12-02 21:23:41 +0100

    updateshoal na gpu

--- Shoal3Cuda/Shoal3Cuda/kernel.cu
+++ Shoal3Cuda/Shoal3Cuda/kernel.cu
@@ -34,16 +34,21 @@ float arrayOfFishes[5 * LEN * LEN];
 
 using namespace std;
 
+__host__ __device__
 float getAngle(float x1, float y1, float x2, float y2) {
 	float value = x1 * x2 + y1 * y2;
 	value = value != 0 ? value / sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)) : 0;
 	return 180 - atan2(-y1, x1) * 57.0;
 }
+
+__host__ __device__
 float vectorMultiply(float x1, float y1, float x2, float y2) {
 	float value = x1 * x2 + y1 * y2;
 	value = value != 0 ? value / sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)) : 0;
 	return value;
 }
+
+__host__ __device__
 float getVectorLength(float x1, float y1, float x2, float y2) {
 	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
@@ -63,7 +68,7 @@ void defineFishes() {
 float* getArrayOfFishes() {
 	return arrayOfFishes;
 }
-
+__host__
 void updateInNeighborhoud(int x, int y) {
 	float nei = 0.2;
 	float neiClose = 0.1;
@@ -142,6 +147,139 @@ void updateInNeighborhoud(int x, int y) {
 	arrayOfFishes[x * LEN * 5 + y + 4] = currentY;
 
 }
+
+__device__
+void updateInNeighborhoudGpu(float *d_arrayOfFishes, int x, int y) {
+	float nei = 0.2;
+	float neiClose = 0.1;
+	int friends[LEN * LEN];
+	int toClose[LEN * LEN];
+	int friendsInd = 0;
+	int toCloseInd = 0;
+	float currentId = d_arrayOfFishes[x * LEN * 5 + y];
+	float currentDirX = d_arrayOfFishes[x * LEN * 5 + y + 1];
+	float currentDirY = d_arrayOfFishes[x * LEN * 5 + y + 2];
+	float currentX = d_arrayOfFishes[x * LEN * 5 + y + 3];
+	float currentY = d_arrayOfFishes[x * LEN * 5 + y + 4];
+
+	for (int i = 0; i < LEN; i++) {
+		for (int j = 0; j < 5 * LEN; j += 5) {
+			float sFishId = d_arrayOfFishes[i * LEN * 5 + j];
+			float sFishDirX = d_arrayOfFishes[i * LEN * 5 + j + 1];
+			float sFishDirY = d_arrayOfFishes[i * LEN * 5 + j + 2];
+			float sFishX = d_arrayOfFishes[i * LEN * 5 + j + 3];
+			float sFishY = d_arrayOfFishes[i * LEN * 5 + j + 4];
+
+			if (getVectorLength(sFishX, sFishY, currentX, currentY) < nei
+				&& currentId != sFishId
+				&& vectorMultiply(sFishX - currentX, sFishY - currentY, currentDirX, currentDirY) > 0) {
+				friends[friendsInd++] = (i * LEN * 5 + j);
+			}
+			if (getVectorLength(sFishX, sFishY, currentX, currentY) < neiClose
+				&& currentId != sFishId
+				&& vectorMultiply(sFishX - currentX, sFishY - currentY, currentDirX, currentDirY) > 0) {
+				toClose[toCloseInd++] = (i * LEN * 5 + j);
+			}
+		}
+	}
+	float dirXNew = 0.0;
+	float posNewX = 0.0;
+	float posNewY = 0.0;
+	float dirYNew = 0.0;
+	float awayFromX = 0.0;
+	float awayFromY = 0.0;
+	for (int i = 0; i < friendsInd; i++) {
+		dirXNew += d_arrayOfFishes[friends[i] + 1];
+		dirYNew += d_arrayOfFishes[friends[i] + 2];
+
+		posNewX += d_arrayOfFishes[friends[i] + 3];
+		posNewY += d_arrayOfFishes[friends[i] + 4];
+
+	}
+	for (int i = 0; i < toCloseInd; i++) {
+		awayFromX += currentX - d_arrayOfFishes[friends[i] + 3];
+		awayFromY += currentY - d_arrayOfFishes[friends[i] + 4];
+	}
+	if (friendsInd == 0) {
+		return;
+	}
+	if (toCloseInd == 0) {
+		currentDirX += (dirXNew / (float)friendsInd) * 0.1;
+		currentDirY += (dirYNew / (float)friendsInd) * 0.1;
+		currentDirX += (posNewX / (float)friendsInd) * 0.05;
+		currentDirY += (posNewY / (float)friendsInd) * 0.05;
+	}
+	else {
+		currentDirX += (awayFromX / (float)friendsInd) * 5;
+		currentDirY += (awayFromY / (float)friendsInd) * 5;
+	}
+	float vecLen = sqrt(pow(currentDirX, 2) + pow(currentDirY, 2));
+	currentDirX /= vecLen;
+	currentDirY /= vecLen;
+	/*if (toClose.size() != 0) {
+		current.x += current.dirX * 0.005;
+		current.y += current.dirY * 0.005;
+	}*/
+	d_arrayOfFishes[x * LEN * 5 + y] = currentId;
+	d_arrayOfFishes[x * LEN * 5 + y + 1] = currentDirX;
+	d_arrayOfFishes[x * LEN * 5 + y + 2] = currentDirY;
+	d_arrayOfFishes[x * LEN * 5 + y + 3] = currentX;
+	d_arrayOfFishes[x * LEN * 5 + y + 4] = currentY;
+
+}
+
+__global__
+void updateShoalGpu(float *d_arrayOfFishes ) {
+	for (int i = 0; i < LEN * LEN * 5; i += 5) {
+		float sFishId = d_arrayOfFishes[i];
+		float sFishDirX = d_arrayOfFishes[i + 1];
+		float sFishDirY = d_arrayOfFishes[i + 2];
+		float sFishX = d_arrayOfFishes[i + 3];
+		float sFishY = d_arrayOfFishes[i + 4];
+
+
+		if (sFishX <= -0.9 && sFishDirX < 0) {
+			sFishDirX = -sFishDirX;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+			//sFishdirY = 1.0;
+		}
+		if (sFishX >= 0.9 && sFishDirX > 0) {
+			sFishDirX = -sFishDirX;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+			//sFishdirY = -1.0;
+		}
+		if (sFishY <= -0.9 && sFishDirY < 0) {
+			//sFishdirX = -1.0;
+			sFishDirY = -sFishDirY;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+		}
+		if (sFishY >= 0.9 && sFishDirY > 0) {
+			//sFishdirX = 1.0;
+			sFishDirY = -sFishDirY;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+		}
+		sFishX += sFishDirX * 0.005;
+		sFishY += sFishDirY * 0.005;
+
+		d_arrayOfFishes[i] = sFishId;
+		d_arrayOfFishes[i + 1] = sFishDirX;
+		d_arrayOfFishes[i + 2] = sFishDirY;
+		d_arrayOfFishes[i + 3] = sFishX;
+		d_arrayOfFishes[i + 4] = sFishY;
+
+	}
+	for (int i = 0; i < LEN; i++) {
+		for (int j = 0; j < 5 * LEN; j += 5) {
+			int place = i * LEN * 5 + j;
+			updateInNeighborhoudGpu(d_arrayOfFishes, i, j);
+		}
+	}
+}
+
 
 void updateShoal() {
 	for (int i = 0; i < LEN * LEN * 5; i += 5) {
