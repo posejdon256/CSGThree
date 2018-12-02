376c06c1a1916c5d893d4e3fe2a2bcf25130737e
jakubiakm <maciej190509@gmail.com>
2018-12-02 22:31:45 +0100

    wersja lepiej działająca

--- Shoal3Cuda/Shoal3Cuda/kernel.cu
+++ Shoal3Cuda/Shoal3Cuda/kernel.cu
@@ -149,26 +149,26 @@ void updateInNeighborhoud(int x, int y) {
 }
 
 __device__
-void updateInNeighborhoudGpu(float *d_arrayOfFishes, int ind) {
+void updateInNeighborhoudGpu(float *d_arrayOfFishes, float *d_arrayOfFishesRead, int ind) {
 	float nei = 0.2;
 	float neiClose = 0.1;
 	int friends[LEN * LEN];
 	int toClose[LEN * LEN];
 	int friendsInd = 0;
 	int toCloseInd = 0;
-	float currentId = d_arrayOfFishes[ind];
-	float currentDirX = d_arrayOfFishes[ind + 1];
-	float currentDirY = d_arrayOfFishes[ind + 2];
-	float currentX = d_arrayOfFishes[ind + 3];
-	float currentY = d_arrayOfFishes[ind + 4];
+	float currentId = d_arrayOfFishesRead[ind];
+	float currentDirX = d_arrayOfFishesRead[ind + 1];
+	float currentDirY = d_arrayOfFishesRead[ind + 2];
+	float currentX = d_arrayOfFishesRead[ind + 3];
+	float currentY = d_arrayOfFishesRead[ind + 4];
 
 	for (int i = 0; i < LEN; i++) {
 		for (int j = 0; j < 5 * LEN; j += 5) {
-			float sFishId = d_arrayOfFishes[i * LEN * 5 + j];
-			float sFishDirX = d_arrayOfFishes[i * LEN * 5 + j + 1];
-			float sFishDirY = d_arrayOfFishes[i * LEN * 5 + j + 2];
-			float sFishX = d_arrayOfFishes[i * LEN * 5 + j + 3];
-			float sFishY = d_arrayOfFishes[i * LEN * 5 + j + 4];
+			float sFishId = d_arrayOfFishesRead[i * LEN * 5 + j];
+			float sFishDirX = d_arrayOfFishesRead[i * LEN * 5 + j + 1];
+			float sFishDirY = d_arrayOfFishesRead[i * LEN * 5 + j + 2];
+			float sFishX = d_arrayOfFishesRead[i * LEN * 5 + j + 3];
+			float sFishY = d_arrayOfFishesRead[i * LEN * 5 + j + 4];
 
 			if (getVectorLength(sFishX, sFishY, currentX, currentY) < nei
 				&& currentId != sFishId
@@ -189,16 +189,16 @@ void updateInNeighborhoudGpu(float *d_arrayOfFishes, int ind) {
 	float awayFromX = 0.0;
 	float awayFromY = 0.0;
 	for (int i = 0; i < friendsInd; i++) {
-		dirXNew += d_arrayOfFishes[friends[i] + 1];
-		dirYNew += d_arrayOfFishes[friends[i] + 2];
+		dirXNew += d_arrayOfFishesRead[friends[i] + 1];
+		dirYNew += d_arrayOfFishesRead[friends[i] + 2];
 
-		posNewX += d_arrayOfFishes[friends[i] + 3];
-		posNewY += d_arrayOfFishes[friends[i] + 4];
+		posNewX += d_arrayOfFishesRead[friends[i] + 3];
+		posNewY += d_arrayOfFishesRead[friends[i] + 4];
 
 	}
 	for (int i = 0; i < toCloseInd; i++) {
-		awayFromX += currentX - d_arrayOfFishes[friends[i] + 3];
-		awayFromY += currentY - d_arrayOfFishes[friends[i] + 4];
+		awayFromX += currentX - d_arrayOfFishesRead[friends[i] + 3];
+		awayFromY += currentY - d_arrayOfFishesRead[friends[i] + 4];
 	}
 	if (friendsInd == 0) {
 		return;
@@ -229,55 +229,13 @@ void updateInNeighborhoudGpu(float *d_arrayOfFishes, int ind) {
 }
 
 __global__
-void updateShoalGpu(float *d_arrayOfFishes ) {
+void updateShoalGpu(float *d_arrayOfFishes, float *d_arrayOfFishesRead ) {
 	const long numThreads = blockDim.x * gridDim.x;
 	const long threadID = blockIdx.x * blockDim.x + threadIdx.x;
 
-	for (int i = threadID; i < LEN * LEN * 5; i += numThreads + 5) {
-		float sFishId = d_arrayOfFishes[i];
-		float sFishDirX = d_arrayOfFishes[i + 1];
-		float sFishDirY = d_arrayOfFishes[i + 2];
-		float sFishX = d_arrayOfFishes[i + 3];
-		float sFishY = d_arrayOfFishes[i + 4];
-
-
-		if (sFishX <= -0.9 && sFishDirX < 0) {
-			sFishDirX = -sFishDirX;
-			sFishX += sFishDirX * 0.01;
-			sFishY += sFishDirY * 0.01;
-			//sFishdirY = 1.0;
-		}
-		if (sFishX >= 0.9 && sFishDirX > 0) {
-			sFishDirX = -sFishDirX;
-			sFishX += sFishDirX * 0.01;
-			sFishY += sFishDirY * 0.01;
-			//sFishdirY = -1.0;
-		}
-		if (sFishY <= -0.9 && sFishDirY < 0) {
-			//sFishdirX = -1.0;
-			sFishDirY = -sFishDirY;
-			sFishX += sFishDirX * 0.01;
-			sFishY += sFishDirY * 0.01;
-		}
-		if (sFishY >= 0.9 && sFishDirY > 0) {
-			//sFishdirX = 1.0;
-			sFishDirY = -sFishDirY;
-			sFishX += sFishDirX * 0.01;
-			sFishY += sFishDirY * 0.01;
-		}
-		sFishX += sFishDirX * 0.005;
-		sFishY += sFishDirY * 0.005;
-
-		d_arrayOfFishes[i] = sFishId;
-		d_arrayOfFishes[i + 1] = sFishDirX;
-		d_arrayOfFishes[i + 2] = sFishDirY;
-		d_arrayOfFishes[i + 3] = sFishX;
-		d_arrayOfFishes[i + 4] = sFishY;
-
-	}
 	for (int i = threadID; i < LEN * LEN * 5; i += numThreads + 5) 
 	{
-		updateInNeighborhoudGpu(d_arrayOfFishes, i);
+		updateInNeighborhoudGpu(d_arrayOfFishes, d_arrayOfFishesRead, i);
 	}
 }
 
@@ -369,14 +327,59 @@ void renderShoal() {
 }//
 void renderGpu()
 {
-	float *d_fishes;
+	for (int i = 0; i < LEN * LEN * 5; i += 5) {
+		float sFishId = arrayOfFishes[i];
+		float sFishDirX = arrayOfFishes[i + 1];
+		float sFishDirY = arrayOfFishes[i + 2];
+		float sFishX = arrayOfFishes[i + 3];
+		float sFishY = arrayOfFishes[i + 4];
+
+
+		if (sFishX <= -0.9 && sFishDirX < 0) {
+			sFishDirX = -sFishDirX;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+			//sFishdirY = 1.0;
+		}
+		if (sFishX >= 0.9 && sFishDirX > 0) {
+			sFishDirX = -sFishDirX;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+			//sFishdirY = -1.0;
+		}
+		if (sFishY <= -0.9 && sFishDirY < 0) {
+			//sFishdirX = -1.0;
+			sFishDirY = -sFishDirY;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+		}
+		if (sFishY >= 0.9 && sFishDirY > 0) {
+			//sFishdirX = 1.0;
+			sFishDirY = -sFishDirY;
+			sFishX += sFishDirX * 0.01;
+			sFishY += sFishDirY * 0.01;
+		}
+		sFishX += sFishDirX * 0.005;
+		sFishY += sFishDirY * 0.005;
+
+		arrayOfFishes[i] = sFishId;
+		arrayOfFishes[i + 1] = sFishDirX;
+		arrayOfFishes[i + 2] = sFishDirY;
+		arrayOfFishes[i + 3] = sFishX;
+		arrayOfFishes[i + 4] = sFishY;
+
+	}
+	float *d_fishes, *d_fishes_read;
 	cudaMalloc((void**)&d_fishes, 5 * LEN * LEN * sizeof(float));
+	cudaMalloc((void**)&d_fishes_read, 5 * LEN * LEN * sizeof(float));
 	cudaMemcpy(d_fishes, arrayOfFishes, LEN * LEN * 5 * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_fishes_read, arrayOfFishes, LEN * LEN * 5 * sizeof(float), cudaMemcpyHostToDevice);
 	cudaDeviceSynchronize();
-	updateShoalGpu << <1024, 1024 >> > (d_fishes);
+	updateShoalGpu << <1024, 1024 >> > (d_fishes, d_fishes_read);
 	cudaDeviceSynchronize();
 	cudaMemcpy(arrayOfFishes, d_fishes, LEN * LEN * 5 * sizeof(float), cudaMemcpyDeviceToHost);
-
+	cudaFree(d_fishes);
+	cudaFree(d_fishes_read);
 }
 
 void render()
